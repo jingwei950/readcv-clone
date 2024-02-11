6cb412a214abb2a5442f1a528f7a3453e943b5bf
Jing Wei <jingwei950@hotmail.sg>
2024-02-11 16:12:56 +0800

    Feat(nav-buttons): Add tooltips to buttons

--- src/app/shared/components/nav-buttons/nav-buttons.component.ts
+++ src/app/shared/components/nav-buttons/nav-buttons.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Output, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+// Spartan-ng imports
 import { HlmButtonDirective } from '../../../../../spartan-ng-components/ui-button-helm/src';
 import { BrnDialogContentDirective, BrnDialogTriggerDirective } from '@spartan-ng/ui-dialog-brain';
 import {
@@ -11,6 +12,12 @@ import {
   HlmDialogHeaderComponent,
   HlmDialogTitleDirective,
 } from '../../../../../spartan-ng-components/ui-dialog-helm/src';
+import { BrnTooltipContentDirective } from '@spartan-ng/ui-tooltip-brain';
+import {
+  HlmTooltipComponent,
+  HlmTooltipTriggerDirective,
+} from '../../../../../spartan-ng-components/ui-tooltip-helm/src';
+import { NavButtonObj } from '../../models/nav-button.model';
 
 @Component({
   selector: 'App-nav-buttons',
@@ -18,23 +25,25 @@ import {
   imports: [
     RouterLink,
     HlmButtonDirective,
+    HlmDialogTitleDirective,
     BrnDialogContentDirective,
     BrnDialogTriggerDirective,
-    HlmDialogComponent,
-    HlmDialogContentComponent,
+    BrnTooltipContentDirective,
+    HlmTooltipTriggerDirective,
     HlmDialogDescriptionDirective,
+    HlmDialogComponent,
+    HlmTooltipComponent,
     HlmDialogFooterComponent,
     HlmDialogHeaderComponent,
-    HlmDialogTitleDirective,
+    HlmDialogContentComponent,
   ],
   templateUrl: './nav-buttons.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavButtonsComponent {
-  iconRoute = input.required<string>();
-  iconAlias = input.required<string>();
-  icon = input.required<string>();
+  icon = input.required<NavButtonObj>();
+  iconType = input.required<string>();
 
   @Output() onClick = new EventEmitter<string>();
   @Output() openDialog = new EventEmitter();

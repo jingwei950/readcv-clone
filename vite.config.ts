e74fb8de5f1ad37d00fb1c1c8fa02282dbd1782d
Jing Wei <jingwei950@hotmail.sg>
2025-01-31 17:37:32 +0800

    feat(config): Add vite-tsconfig-paths plugin to enhance module resolution in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [analog(), viteTsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',

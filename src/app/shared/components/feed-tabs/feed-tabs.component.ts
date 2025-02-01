c832326ea325cc4581fec47eec51634e5c497502
Jing Wei <jingwei950@hotmail.sg>
2025-02-01 22:45:42 +0800

    feat(feed-tabs): Implement FeedTabsComponent for dynamic navigation with filter state management based on selected tabs

---
+++ src/app/shared/components/feed-tabs/feed-tabs.component.ts
@@ -0,0 +1,66 @@
+import { NgClass } from '@angular/common';
+import { HlmButtonModule } from '@spartan-ng/ui-button-helm';
+import { NavigationService } from '@services/navigation.service';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'App-feed-tabs',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgClass, HlmButtonModule],
+  template: `
+    <div
+      class="flex w-full border-b border-gray-400 items-center min-h-16 px-6 sticky top-0 bg-background z-50"
+      [ngClass]="
+        navService.navState() === 'msg' || navService.navState() === 'bookmark' ? 'justify-start' : 'justify-center'
+      "
+    >
+      @if (navService.navState() === 'home') {
+        @for (button of filterButtons; track $index) {
+          <button
+            hlmBtn
+            variant="link"
+            (click)="changeFilterState(button === 'highlights' ? 'highlights' : 'everyone', 'home')"
+            class="capitalize"
+            [ngClass]="{
+              '!text-gray-500': navService.filterState() !== button,
+            }"
+          >
+            {{ button }}
+          </button>
+        }
+      } @else if (navService.navState() === 'noti') {
+        @for (button of notiButtons; track $index) {
+          <button
+            hlmBtn
+            variant="link"
+            (click)="changeFilterState(button === 'all' ? 'all' : 'mentions', 'notification')"
+            class="capitalize"
+            [ngClass]="{
+              '!text-gray-500': navService.notificationFilterState() !== button,
+            }"
+          >
+            {{ button }}
+          </button>
+        }
+      } @else if (navService.navState() === 'msg') {
+        <p class="font-medium text-sm text-primary">Replies</p>
+      } @else if (navService.navState() === 'bookmark') {
+        <p class="font-medium text-sm text-primary">Bookmarks</p>
+      }
+    </div>
+  `,
+})
+export class FeedTabsComponent {
+  navService = inject(NavigationService);
+
+  notiButtons = ['all', 'mentions'];
+  filterButtons = ['highlights', 'everyone'];
+
+  changeFilterState(state: string, filterType: string) {
+    if (filterType === 'home') {
+      this.navService.filterState.update(() => state);
+    } else if (filterType === 'notification') {
+      this.navService.notificationFilterState.update(() => state);
+    }
+  }
+}

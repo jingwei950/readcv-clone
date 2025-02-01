d3b02eedce2f87153df38843274511b1c30522b4
Jing Wei <jingwei950@hotmail.sg>
2025-02-01 22:43:42 +0800

    refactor(post-feed): Simplify post feed component by replacing extracting code into components, feed-tabs and post-composer

--- src/app/shared/components/post-feed/post-feed.component.ts
+++ src/app/shared/components/post-feed/post-feed.component.ts
@@ -2,89 +2,41 @@ import {
   effect,
   inject,
   Component,
-  ViewChild,
+  viewChild,
   ElementRef,
   WritableSignal,
   ChangeDetectionStrategy,
 } from '@angular/core';
-import { NgClass } from '@angular/common';
 import { Post } from '@models/post.model';
 import { FeedService } from '@services/feed.service';
 import { AuthService } from '@services/auth.service';
-import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { NavigationService } from '@services/navigation.service';
-import { AvatarComponent } from '@components/avatar/avatar.component';
+import { FeedTabsComponent } from '../feed-tabs/feed-tabs.component';
 import { PostCardComponent } from '@components/post-card/post-card.component';
 import { ResponsiveBreakpointService } from '@services/responsive-breakpoint.service';
+import { PostComposerComponent } from '../post-composer/post-composer.component';
 
 @Component({
   selector: 'App-post-feed',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgClass, AvatarComponent, PostCardComponent, HlmButtonDirective],
+  imports: [PostCardComponent, FeedTabsComponent, PostComposerComponent],
   template: `
     <div class="inline-flex justify-center w-full border-x border-gray-400 min-h-screen">
       <div class="flex flex-col w-full">
-        <div
-          class="flex w-full border-b border-gray-400 items-center min-h-16 px-6 sticky top-0 bg-background z-50"
-          [ngClass]="
-            navService.navState() === 'msg' || navService.navState() === 'bookmark' ? 'justify-start' : 'justify-center'
-          "
-        >
-          @if (navService.navState() === 'home') {
-            @for (button of filterButtons; track $index) {
-              <button
-                hlmBtn
-                variant="link"
-                (click)="changeFilterState(button === 'highlights' ? 'highlights' : 'everyone', 'home')"
-                class="capitalize"
-                [ngClass]="{
-                  '!text-gray-500': navService.filterState() !== button,
-                }"
-              >
-                {{ button }}
-              </button>
-            }
-          } @else if (navService.navState() === 'noti') {
-            @for (button of notiButtons; track $index) {
-              <button
-                hlmBtn
-                variant="link"
-                (click)="changeFilterState(button === 'all' ? 'all' : 'mentions', 'notification')"
-                class="capitalize"
-                [ngClass]="{
-                  '!text-gray-500': navService.notificationFilterState() !== button,
-                }"
-              >
-                {{ button }}
-              </button>
-            }
-          } @else if (navService.navState() === 'msg') {
-            <p class="font-medium text-sm text-primary">Replies</p>
-          } @else if (navService.navState() === 'bookmark') {
-            <p class="font-medium text-sm text-primary">Bookmarks</p>
-          }
-        </div>
+        <App-feed-tabs />
+
         @if (currentUser()) {
-          <div class="flex items-center gap-3 px-6 py-5 border-b border-gray-400 min-h-32">
-            <App-avatar buttonVariant="large" name="jingwei" [avatarUrl]="currentUser()?.photoURL" />
-            <textarea
-              #textarea
-              type="text"
-              class="flex-1 !ring-0 w-full px-4 py-2 !outline-none resize-none h-fit min-h-fit"
-              placeholder="What's on your mind..."
-            ></textarea>
-          </div>
+          <App-post-composer [currentUser]="currentUser()!" />
         }
+
         @switch (navService.navState()) {
           @case ('home') {
-            @if (navService.filterState() === 'highlights') {
+            @if (navService.filterState() === 'highlights' && allPosts().length > 0) {
               @for (post of allPosts(); track $index) {
-                @if (post) {
-                  <App-post-card [post]="post" />
-                }
+                <App-post-card [post]="post" />
               }
-            } @else {
+            } @else if (navService.filterState() === 'everyone') {
               <p>Everyone</p>
             }
           }
@@ -118,28 +70,17 @@ export class PostFeedComponent {
   navService = inject(NavigationService);
   screenSize = inject(ResponsiveBreakpointService);
 
-  @ViewChild('textarea') textareaRef?: ElementRef;
-
-  notiButtons = ['all', 'mentions'];
-  filterButtons = ['highlights', 'everyone'];
+  textareaRef = viewChild(ElementRef);
 
   allPosts: WritableSignal<Post[]> = this.feedService.allPosts;
   currentUser = this.authService.auth_user;
 
   constructor() {
     effect(() => {
-      this.textareaRef?.nativeElement.addEventListener('input', () => {
-        this.textareaRef!.nativeElement.style.height = 'auto';
-        this.textareaRef!.nativeElement.style.minHeight = `${this.textareaRef!.nativeElement.scrollHeight}px`;
+      this.textareaRef()?.nativeElement.addEventListener('input', () => {
+        this.textareaRef()!.nativeElement.style.height = 'auto';
+        this.textareaRef()!.nativeElement.style.minHeight = `${this.textareaRef()!.nativeElement.scrollHeight}px`;
       });
     });
-  }
-
-  changeFilterState(state: string, filterType: string) {
-    if (filterType === 'home') {
-      this.navService.filterState.update(() => state);
-    } else if (filterType === 'notification') {
-      this.navService.notificationFilterState.update(() => state);
-    }
   }
 }

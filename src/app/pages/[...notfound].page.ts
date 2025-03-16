bc828ce8a713bcae9e7d7150e79b4904ffef2da1
Jing Wei <jingwei950@hotmail.sg>
2025-03-16 22:07:33 +0800

    feat(notfound): Add route metadata for 404 handling with SSR support and logging

--- src/app/pages/[...notfound].page.ts
+++ src/app/pages/[...notfound].page.ts
@@ -1,9 +1,26 @@
 // Angular imports
+import { RouteMeta } from '@analogjs/router';
+import { injectResponse } from '@analogjs/router/tokens';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 // 3rd party imports
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+
+export const routeMeta: RouteMeta = {
+  title: 'Page Not Found',
+  canActivate: [
+    () => {
+      const response = injectResponse();
+      if (import.meta.env.SSR && response) {
+        console.log('Page Not Found');
+        response.statusCode = 404;
+        response.end();
+      }
+      return true;
+    },
+  ],
+};
 
 @Component({
   standalone: true,
